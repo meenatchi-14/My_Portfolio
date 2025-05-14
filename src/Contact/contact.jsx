e09262da523bcc523860b1c3a31d06cb2529a930
meenatchi <meenatchicse44@gmail.com>
2025-05-14 19:07:45 +0530

    emailjs

--- src/Contact/contact.jsx
+++ src/Contact/contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     emailjs
       .sendForm(
         "service_vtdr3qn",
-        "template_xsns0ja",
+        "template_jozqjw9",
         form.current,
         "AY133yB7MjAu5iJRJ"
       )
